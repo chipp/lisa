82eefe55468413dc2ada91c932b0b83d2686de7c
Vladimir Burdukov <chippcheg@gmail.com>
2020-05-08 23:52:13 +0300

    Filtered non-POST requests

--- lisa/src/main.rs
+++ lisa/src/main.rs
@@ -1,37 +1,44 @@
-use std::convert::Infallible;
-
 use bytes::buf::BufExt;
 
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{header, Body, Request, Response, Server, StatusCode};
+use hyper::{header, Body, Method, Request, Response, Server, StatusCode};
 
 use alice::{Request as AliceRequest, Response as AliceResponse};
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+type ErasedError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, ErasedError>;
 
 async fn hello(request: Request<Body>) -> Result<Response<Body>> {
-    let body = hyper::body::aggregate(request).await?;
-    let alice_request: AliceRequest = serde_json::from_reader(body.reader())?;
+    match request.method() {
+        &Method::POST => {
+            let body = hyper::body::aggregate(request).await?;
+            let alice_request: AliceRequest = serde_json::from_reader(body.reader())?;
+            let alice_response = if &alice_request.payload.original_utterance == "ping" {
+                AliceResponse::from_string("pong")
+            } else {
+                AliceResponse::from_string("Привет! Температура в зале 23.5 градуса")
+            };
+            let json_response = serde_json::to_vec(&alice_response)?;
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header(header::CONTENT_TYPE, "application/json")
+                .body(Body::from(json_response))?;
+            Ok(response)
+        }
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("invalid request"))?;
 
-    let alice_response = if &alice_request.payload.original_utterance == "ping" {
-        AliceResponse::from_string("pong")
-    } else {
-        AliceResponse::from_string("Привет! Температура в зале 23.5 градуса")
-    };
-
-    let json_response = serde_json::to_vec(&alice_response)?;
-
-    let response = Response::builder()
-        .status(StatusCode::OK)
-        .header(header::CONTENT_TYPE, "application/json")
-        .body(Body::from(json_response))?;
-
-    Ok(response)
+            Ok(response)
+        }
+    }
 }
 
 #[tokio::main]
-pub async fn main() -> Result<()> {
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello)) });
+async fn main() -> Result<()> {
+
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, ErasedError>(service_fn(hello)) });
 
     let addr = ([0, 0, 0, 0], 8080).into();
     let server = Server::bind(&addr).serve(make_svc);
